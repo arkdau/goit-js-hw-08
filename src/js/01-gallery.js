a7bffc5a93d17ce6cb637226e83f2913958fd8fd
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-16 21:31:57 +0200

    fix: fel type=text/css

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,22 +1,22 @@
-// Add imports above this line
-// import SimpleLightbox from 'simplelightbox';
+// Add imports above this lineimport
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 // Dodatkowy import stylów
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
 
 const list = document.querySelector('.gallery');
 const markup = galleryItems.map((item) => `<li><a  class="gallery__item" href=${item.original}><img  class="gallery__image"  src=${item.preview} alt="${item.description}" width="480"  height=auto></a></li>`).join('');
 list.insertAdjacentHTML('beforeend', markup);
-// console.log();
+console.log();
 
-// const gallery = new SimpleLightbox('.gallery a', {
-//   captions: true,
-//   captionPosition: 'outside',
-//   captionType: 'attr',
-//   captionsData: 'alt',
-//   captionDelay: 250,
-//   showCounter: true,
-// });
+const gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionPosition: 'outside',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+  showCounter: true,
+});
