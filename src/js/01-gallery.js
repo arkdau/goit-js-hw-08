0f424f7f8b73ad4fadf082d3a10a0123aa02b8f1
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-16 21:46:50 +0200

    test 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,10 +7,10 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-const list = document.querySelector('.gallery');
-const markup = galleryItems.map((item) => `<li><a  class="gallery__item" href=${item.original}><img  class="gallery__image"  src=${item.preview} alt="${item.description}" width="480"  height=auto></a></li>`).join('');
-list.insertAdjacentHTML('beforeend', markup);
-console.log();
+// const list = document.querySelector('.gallery');
+// const markup = galleryItems.map((item) => `<li><a  class="gallery__item" href=${item.original}><img  class="gallery__image"  src=${item.preview} alt="${item.description}" width="480"  height=auto></a></li>`).join('');
+// list.insertAdjacentHTML('beforeend', markup);
+// console.log();
 
 // const gallery = new SimpleLightbox('.gallery a', {
 //   captions: true,
