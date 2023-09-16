6c0c9854552c0b223d8146a8230301a097448508
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-16 03:40:38 +0200

    import simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from 'simplelightbox';
 // Dodatkowy import stylów
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
