7e7e786f84b4ee9a3ef7af4fc6454c4c886abbe2
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-16 18:19:07 +0200

    setting: gh-pages

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -10,7 +10,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const list = document.querySelector('.gallery');
 const markup = galleryItems.map((item) => `<li><a  class="gallery__item" href=${item.original}><img  class="gallery__image"  src=${item.preview} alt="${item.description}" width="480"  height=auto></a></li>`).join('');
 list.insertAdjacentHTML('beforeend', markup);
-console.log();
+// console.log();
 
 const gallery = new SimpleLightbox('.gallery a', {
   captions: true,
