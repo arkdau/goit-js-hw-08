724902cdbbc5230012ef536e95cd68a430e937d4
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-17 18:22:47 +0200

    Task 2 - done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,7 @@
 import Player from '@vimeo/player';
+// Requiring lodash library
+const _throttle = require("lodash.throttle");
+import { default as localStorage } from './storage';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -10,3 +13,31 @@ player.on('play', function () {
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
+
+player.on('play', (data) => {
+  //console.log('Player start: ', data);
+  let videoplayerCurrentTime = 0;
+  videoplayerCurrentTime = localStorage.load("videoplayer-current-time");
+  console.log(`videoplayer-current-time: ${videoplayerCurrentTime} sec`);
+  player.setCurrentTime(videoplayerCurrentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+     seconds = this;
+  }).catch(function(error) {
+      switch (error.name){
+        case 'RangeError':
+            // the time was less than 0 or greater than video's duration
+            break;
+        default:
+            // some other error occurred
+            break;
+      }
+    });
+});
+
+player.on('timeupdate', _throttle((data) => {
+  console.log('time update: ', data);
+  localStorage.save("videoplayer-current-time", data.seconds);
+},1000));
+
+console.log('localStorage: ', localStorage);
+
