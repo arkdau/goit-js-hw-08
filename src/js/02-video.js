fb1b4742392dfa42990234a3b619ff8a783cc08c
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-17 21:22:18 +0200

    fix: formatter code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,43 +1,45 @@
-import Player from '@vimeo/player';
+import Player from "@vimeo/player";
 // Requiring lodash library
 const _throttle = require("lodash.throttle");
-import { default as localStorage } from './storage';
+import { default as localStorage } from "./storage";
 
-const iframe = document.querySelector('iframe');
+const iframe = document.querySelector("iframe");
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
+player.on("play", function () {
+  console.log("played the video!");
 });
 
 player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
+  console.log("title:", title);
 });
 
-player.on('play', (data) => {
+player.on("play", (data) => {
   //console.log('Player start: ', data);
   let videoplayerCurrentTime = 0;
   videoplayerCurrentTime = localStorage.load("videoplayer-current-time");
   console.log(`videoplayer-current-time: ${videoplayerCurrentTime} sec`);
-  player.setCurrentTime(videoplayerCurrentTime).then(function(seconds) {
+  player.setCurrentTime(videoplayerCurrentTime).then(function (seconds) {
     // seconds = the actual time that the player seeked to
-     seconds = this;
-  }).catch(function(error) {
-      switch (error.name){
-        case 'RangeError':
-            // the time was less than 0 or greater than video's duration
-            break;
-        default:
-            // some other error occurred
-            break;
-      }
-    });
+    seconds = this;
+  }).catch(function (error) {
+    switch (error.name) {
+      case "RangeError":
+        // the time was less than 0 or greater than video's duration
+        break;
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 });
 
-player.on('timeupdate', _throttle((data) => {
-  console.log('time update: ', data);
-  localStorage.save("videoplayer-current-time", data.seconds);
-},1000));
+player.on(
+  "timeupdate",
+  _throttle((data) => {
+    console.log("time update: ", data);
+    localStorage.save("videoplayer-current-time", data.seconds);
+  }, 1000),
+);
 
-console.log('localStorage: ', localStorage);
-
+console.log("localStorage: ", localStorage);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,26 @@
-import { default as localStorage } from './storage';
+import { default as localStorage } from "./storage";
 // Requiring lodash library
 const _throttle = require("lodash.throttle");
 
-const textInput = document.querySelector('.feedback-form input');
-const textAreaInput = document.querySelector('.feedback-form textarea');
-const msgForm = document.querySelector('.feedback-form');
+const textInput = document.querySelector(".feedback-form input");
+const textAreaInput = document.querySelector(".feedback-form textarea");
+const msgForm = document.querySelector(".feedback-form");
 
 let msg = {
-  email:'',
-  message:'',
-}
+  email: "",
+  message: "",
+};
 
-function hasStorageItem (storageItem) {
+function hasStorageItem(storageItem) {
   // let msgTmp = localStorage.load("feedback-form-state");
   let msgTmp = localStorage.load(storageItem);
 
-  if  (msgTmp != undefined) {
+  if (msgTmp != undefined) {
     msg = msgTmp;
     return true;
-  };
+  }
   return false;
-};
+}
 
 // if (msg != undefined) {
 //   textInput.value = msg.email;
@@ -36,24 +36,29 @@ function hasStorageItem (storageItem) {
 if (hasStorageItem("feedback-form-state") === true) {
   textInput.value = msg.email;
   textAreaInput.value = msg.message;
-};
+}
 
-textInput.addEventListener("input", _throttle((e) => {
-  if (e.currentTarget != null) {
-    msg.email = e.currentTarget.value;
-    localStorage.save("feedback-form-state", msg);
-    console.log(msg.email);
-  };
-}, 500));
+textInput.addEventListener(
+  "input",
+  _throttle((e) => {
+    if (e.currentTarget != null) {
+      msg.email = e.currentTarget.value;
+      localStorage.save("feedback-form-state", msg);
+      console.log(msg.email);
+    }
+  }, 500),
+);
 
-
-textAreaInput.addEventListener("input", _throttle((e) => {
-  if (e.currentTarget != null) {
-    msg.message = e.currentTarget.value;
-    localStorage.save("feedback-form-state", msg);
-    console.log(msg.message);
-  };
-}, 500));
+textAreaInput.addEventListener(
+  "input",
+  _throttle((e) => {
+    if (e.currentTarget != null) {
+      msg.message = e.currentTarget.value;
+      localStorage.save("feedback-form-state", msg);
+      console.log(msg.message);
+    }
+  }, 500),
+);
 
 msgForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -62,8 +67,7 @@ msgForm.addEventListener("submit", (e) => {
   msg.message = form.elements.message.value;
   localStorage.remove("feedback-form-state");
   form.reset();
-  console.log('feedback-form-state = ', msg);
+  console.log("feedback-form-state = ", msg);
 });
-
 
 console.log(msg);
