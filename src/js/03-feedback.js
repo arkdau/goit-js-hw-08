1996ea15f77ae3ce8fb972cf2f71a5524d0abc21
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-20 09:51:14 +0200

    fix: msgForm.addEventListener()

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,7 +12,6 @@ let msg = {
 };
 
 function hasStorageItem(storageItem) {
-  // let msgTmp = localStorage.load("feedback-form-state");
   let msgTmp = localStorage.load(storageItem);
 
   if (msgTmp != undefined) {
@@ -22,46 +21,18 @@ function hasStorageItem(storageItem) {
   return false;
 }
 
-// if (msg != undefined) {
-//   textInput.value = msg.email;
-//   textAreaInput.value = msg.message;
-// };
-//
-// textInput.addEventListener("input", (e) => {
-//   msg.email = e.currentTarget.value;
-//   localStorage.save("feedback-form-state", msg)
-//   console.log(msg.email);
-// });
-
 if (hasStorageItem("feedback-form-state") === true) {
   textInput.value = msg.email;
   textAreaInput.value = msg.message;
 }
 
-console.log('input: ', msgForm);
-
-msgForm[0].addEventListener(
+msgForm.addEventListener(
   "input",
   _throttle(() => {
-    // if (e.currentTarget != null) {
-      // const form = e.currentTarget;
-      // msg.email = e.currentTarget.value;
       msg.email = textInput.value;
-      localStorage.save("feedback-form-state", msg);
-      console.log(msg.email);
-    // }
-  }, 500),
-);
-
-msgForm[1].addEventListener(
-  "input",
-  _throttle(() => {
-    // if (e.currentTarget != null) {
-      // msg.message = e.currentTarget.value;
       msg.message = textAreaInput.value;
       localStorage.save("feedback-form-state", msg);
-      console.log(msg.message);
-    // }
+      console.log(msg);
   }, 500),
 );
 
@@ -73,9 +44,9 @@ msgForm.addEventListener("submit", (e) => {
   localStorage.save("feedback-form-state", msg);
 
   if (msg.email === '') {
-    console.log('email - error ');
+    alert('email adres - puste pole.\nUzupełnij brakujące dane.');
   }else if (msg.message === '') {
-    console.log('message - error');
+    alert('Message - puste pole.\nUzupełnij brakujące dane.');
   }else {
     console.log('cache: feedback-form-state = ', msg);
     console.log('localStorage: feedback-form-state = ', localStorage.load("feedback-form-state"));
